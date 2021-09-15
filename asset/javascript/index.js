823b3982a87348aebb7c952f96b2f00a39d6f9b1
sakshimawar117 <sakshimawar10@gmail.com>
2021-09-15 11:57:51 +0530

    js

---
+++ asset/javascript/index.js
@@ -0,0 +1,61 @@
+// declare a variable named challenge and assign it an intial value 'javaScript'
+
+let challenge="javascript"
+
+console.log(challenge)
+
+//pritn the string on the browser console using console.log()
+
+let str = "Hello World!"
+
+console.log(str)
+
+
+//print the lenght of the string
+
+let str2 = "coding".length
+
+console.log(str2)
+
+//Make an object and fill your profile 
+
+let profile = {
+    Name: "Sakshi Mawar",
+    Fathername: "Premkuar Mawar",
+    Mothername: " Manju Mawar ",
+    age:20,
+
+}
+//Make an array of boject in which fill all class memeber value
+console.log(profile)
+
+let classmember = [
+    {
+        Name:" Surbhi Sahu",
+        age:20,
+        Fathername:"Sunil Sahu",
+
+
+        
+    },
+    {
+
+        Name: "Hussain",
+    },
+    {
+        Name:"Aayush jain"
+    },
+
+]
+
+
+
+
+console.log(classmember)
+
+
+console.log(classmember[0])
+
+console.log(classmember[1])
+
+console.log(classmember[2])
