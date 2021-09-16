c00561736acb34dfd4d05d156c029d7b7dd860b8
sakshimawar117 <sakshimawar10@gmail.com>
2021-09-16 13:34:59 +0530

    js

--- asset/javascript/index.js
+++ asset/javascript/index.js
@@ -59,3 +59,39 @@ console.log(classmember[0])
 console.log(classmember[1])
 
 console.log(classmember[2])
+
+//Using console.log() print out the following statement
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up'by John Holmes teaches us to help one another.")
+
+//Using console.log() print out the following quote by Mother Teresa:
+
+console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and 
+love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`)
+
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+
+console.log(typeof '10'==10)
+
+console.log(typeof '10' =='string')
+
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10
+
+console.log(parseFloat('9.8') ==10)
+
+console.log(parseFloat('9.8') === parseFloat('9.8'))
+
+//Generate a random number between 0 and 100 inclusively.Generate a random number between 0 and 100 inclusively.
+
+Math.floor (Math.random()*100)
+console.log(Math.floor (Math.random()*100))
+console.log(Math.floor (Math.random()*100))
+console.log(Math.floor (Math.random()*100))
+console.log(Math.floor (Math.random()*100))
+console.log(Math.floor (Math.random()*100))
+
+
+//Generate a random number between 50 and 100 inclusively.
+
+//Access the 'JavaScript' string characters using a random numb
+
